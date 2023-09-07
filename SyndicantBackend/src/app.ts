e3149366b1378856d579551e6156b14ded1c6e72
Johan Smit <johansm@bbd.co.za>
2023-09-07 19:29:19 +0200

    Bad choices

--- SyndicantBackend/src/app.ts
+++ SyndicantBackend/src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from 'express';
 import 'dotenv/config';
 import neo4j from 'neo4j-driver';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const {
   DB_LINK = '',
@@ -14,6 +15,10 @@ const driver = neo4j.driver(DB_LINK, neo4j.auth.basic(DB_USER, DB_PASS))
 const session = driver.session();
 
 const app: Application = express();
+
+app.use(cors({
+  origin: '*'
+}));
 app.use(bodyParser.json());
 
 app.get('/health', (req: Request, res: Response): void => {
